4d5e5aec91c86110018c100af2d7860a65edd109
Rutronik Elektronische Bauelemente GmbH <gdr@rutronik.com>
2022-11-16 15:45:12 +0200

    Date change. Test.

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 * Related Document: See README.md
 *
 *
-*  Created on: 2022-10-15
+*  Created on: 2022-10-16
 *  Company: Rutronik Elektronische Bauelemente GmbH
 *  Address: Jonavos g. 30, Kaunas 44262, Lithuania
 *  Author: GDR
